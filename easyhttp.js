861f7627a4db2277f23353821746874f771fca79
Romila Rangel <85246670+romilagui@users.noreply.github.com>
2022-07-13 15:43:17 +0100

    Add files via upload

---
+++ easyhttp.js
@@ -0,0 +1,36 @@
+// ES5 Object Oriented Library
+
+// We are using prototypes
+
+function easyHTTP(){
+  this.http = new XMLHttpRequest();
+
+}
+// Make an HTTP Get Request
+easyHTTP.prototype.get = function(url, callback) {
+  this.http.open('GET', url, true);
+
+  let self = this;
+  this.http.onload = function() {
+    if(self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback('Error:' + self.http.status);
+    }
+  }
+  this.http.send();
+}
+
+// Make an HTTP POST Request
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+  let self = this;
+  this.http.onload = function() {
+      callback(null, self.http.responseText);
+  }
+  this.send(JSON.stringify(data));
+}
+// Make an HTTP PUT Request
+
+// Make an HTTP DELETE Request
